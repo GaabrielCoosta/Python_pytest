6bfc1721e47d62721494565d7e2ac2556af3141f
Gabriel Costa Andrade <americanogabrielandrade@gmail.com>
2023-03-22 20:16:57 -0300

    Adicionado Itens
    Adicionando arquivos necessarios para rodar o codigo da atividade

---
+++ File/testemult.py
@@ -0,0 +1,36 @@
+import pytest
+from verificanum import multiplos_ambos, multiplos_vazio, multiplos5, multiplos7
+
+
+class test_multiplos:
+    def setup_method(self):
+        pass
+    
+    def test_multiplos5(self):
+        resultado1 = multiplos5()
+        assert resultado1 == 'fizz'
+        
+        with pytest.raises(ValueError):
+            print("Por favor digite um número natural")
+
+    def test_multiplos7(self):
+        resultado2 = multiplos7()
+        assert resultado2 == 'buzz'
+        
+        with pytest.raises(ValueError):
+            print("Por favor digite um número natural")
+    
+    def test_multiplos_ambos(self):
+        resultado3 = multiplos_ambos()
+        assert resultado3 == 'fizzbuzz'
+        
+        with pytest.raises(ValueError):
+            print("Por favor digite um número natural")
+    
+    def test_multiplos_vazio(self):
+        resultado4 = multiplos_vazio()
+        assert resultado4 == 'miss'
+        
+        with pytest.raises(ValueError):
+            print("Por favor digite um número natural")
+       
---
+++ File/verificanum.py
@@ -0,0 +1,70 @@
+
+def multiplos_ambos():
+    
+    try:
+        a = int(input("Digite um número natural: "))
+        
+        if (a % 5 == 0) and (a % 7 == 0):
+            resultado = print('fizzbuzz')
+            return resultado
+        
+        else:
+            resultado = print('Nuúmero digitado não é multiplo de 5 e nem de 7!')
+            return resultado
+    
+    except TypeError as e:
+        resultado = f'''Você precisa informar um número natural.
+        Por favor, digite um número posito e inteiro! - {e}''' 
+    return resultado 
+
+def multiplos5():
+    
+    try:
+        a = int(input("Digite um número natural: "))
+        
+        if a % 5 == 0:
+            resultado = print('fizz')
+            return resultado
+        
+        else:
+            resultado = print('Número digitado não é multiplo de 5!')
+            return resultado
+    
+    except TypeError as e:
+        resultado = f'''Você precisa informar um número natural.
+        Por favor, digite um número posito e inteiro! - {e}'''
+    return resultado 
+
+def multiplos7():
+    
+    try:
+        a = int(input("Digite um número natural: "))
+        
+        if a % 7 == 0:
+            resultado = print('buzz')
+            return resultado
+        else:
+            resultado = print('Número digitado não é multiplo de 7!')
+            return resultado
+    
+    except TypeError as e:
+        resultado = f'''Você precisa informar um número natural.
+        Por favor, digite um número posito e inteiro! - {e}''' 
+    return resultado 
+
+def multiplos_vazio():
+    
+    try:
+        a = int(input("Digite um número natural: "))
+        
+        if (a % 7 != 0) and (a % 5 != 0):
+            resultado = print('miss')
+            return resultado
+        
+    
+    except TypeError as e:
+        resultado = f'''Você precisa informar um número natural.
+        Por favor, digite um número posito e inteiro! - {e}''' 
+    return resultado
+
+multiplos5()
